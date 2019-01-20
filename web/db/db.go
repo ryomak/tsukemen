c13ec9634f40841545ef06b736418bab833b4a12
ryomak <ryooomaaa0413@outlook.com>
2019-01-20 15:24:28 +0900

    fix

--- web/db/db.go
+++ web/db/db.go
@@ -1,41 +1,33 @@
 package db
 
 import (
-	"github.com/gocql/gocql"
+	"github.com/jinzhu/gorm"
 	"github.com/ryomak/tsukemen/web/model"
 )
 
 type DBSession struct {
-	DB *gocql.Session
+	DB *gorm.DB
 }
 
 func NewDBSession() *DBSession {
-	cluster := gocql.NewCluster("localhost:7000", "localhost:7001")
-	cluster.Keyspace = "example"
-	cluster.Consistency = gocql.Quorum
-	session, err := cluster.CreateSession()
+	db, err := gorm.Open("mysql", "test.db")
 	if err != nil {
-		panic(err)
+		panic("failed to connect database")
 	}
-	defer session.Close()
+	defer db.Close()
 
-	return &DBSession{DB: session}
+	return &DBSession{DB: db}
 }
 
 func (d *DBSession) VoteForCandidate(v model.Vote) error {
-	if err := d.DB.Query(`INSERT INTO tweet (user, candidate_id) VALUES (?, ?)`, v.User, v.CandidateID).Exec(); err != nil {
+	if err := d.DB.Create(&v).Error; err != nil {
 		return err
 	}
 	return nil
 }
 func (d *DBSession) Result() ([]model.Vote, error) {
 	result := make([]model.Vote, 0)
-	vote := model.Vote{}
-	iter := d.DB.Query(`SELECT user,cadidate_id FROM votes`).Iter()
-	for iter.Scan(&vote.User, &vote.CandidateID) {
-		result = append(result, vote)
-	}
-	if err := iter.Close(); err != nil {
+	if err := d.DB.Find(&result).Error; err != nil {
 		return nil, err
 	}
 	return result, nil
